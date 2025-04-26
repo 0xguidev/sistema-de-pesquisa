cf2bdb9fdcf8d9a14d7eff0f3c32f6a22fa5471c
0xguidev <guilhermeviniciustrabalho@gmail.com>
2025-04-26 13:55:12 -0600

    fix: update accountId generation in makeSurvey function

--- test/factories/make-survey.ts
+++ test/factories/make-survey.ts
@@ -1,4 +1,7 @@
+import { PrismaSurveyMapper } from '@/infra/database/prisma/mappers/prisma-survey-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
 import { Survey, SurveyProps } from 'src/domain/entities/survey'
 
@@ -11,11 +14,26 @@ export function makeSurvey(
       title: faker.lorem.sentence(),
       type: faker.lorem.word(),
       location: faker.lorem.word(),
-      accountId: new UniqueEntityID('account-id'),
+      accountId: new UniqueEntityID(),
       ...override,
     },
     id,
   )
 
   return survey
+}
+
+@Injectable()
+export class SurveyFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaSurvey(data: Partial<Survey> = {}): Promise<Survey> {
+    const survey = makeSurvey(data)
+
+    await this.prisma.survey.create({
+      data: PrismaSurveyMapper.toPrisma(survey),
+    })
+
+    return survey
+  }
 }
