8b093a6a092832bec50f8a5b197f2d905a471f17
0xguidev <guilhermeviniciustrabalho@gmail.com>
2025-08-10 13:59:08 -0300

    refactor: simplify CreateSurveyUseCase by removing question handling logic

--- src/domain/use-cases/survey/create-survey.ts
+++ src/domain/use-cases/survey/create-survey.ts
@@ -1,31 +1,17 @@
-import { Injectable } from '@nestjs/common'
-import { Either, right } from '@/core/types/either'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-
-import { Survey } from '@/domain/entities/survey'
-import { Question } from '@/domain/entities/question'
-import { OptionAnswer } from '@/domain/entities/option-answer'
-
-import { SurveyRepository } from '@/domain/repositories/survey-repository'
-import { QuestionRepository } from '@/domain/repositories/question-repository'
-import { OptionAnswerRepository } from '@/domain/repositories/option-answer-repository'
+import { Injectable } from '@nestjs/common'
+import { Either, right } from 'src/core/types/either'
+import { Survey } from 'src/domain/entities/survey'
+import { SurveyRepository } from 'src/domain/repositories/survey-repository'
 
 interface CreateSurveyUseCaseRequest {
   title: string
   location: string
   type: string
   accountId: string
-  questions: Array<{
-    questionTitle: string
-    questionNum: number
-    options: Array<{
-      optionTitle: string
-      optionNum: number
-    }>
-  }>
 }
 
-type CreateSurveyUseCaseResponse = Either<
+type CreateQuestionUseCaseResponse = Either<
   null,
   {
     survey: Survey
@@ -34,50 +20,25 @@ type CreateSurveyUseCaseResponse = Either<
 
 @Injectable()
 export class CreateSurveyUseCase {
-  constructor(
-    private surveyRepository: SurveyRepository,
-    private questionRepository: QuestionRepository,
-    private optionAnswerRepository: OptionAnswerRepository,
-  ) {}
+  constructor(private surveyRepository: SurveyRepository) {}
 
   async execute({
     title,
     location,
     type,
-    accountId
+    accountId,
   }: CreateSurveyUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const survey = Survey.create({
       title,
       location,
       type,
-      accountId: accountIdUnique,
+      accountId: new UniqueEntityID(accountId),
     })
-
 
     await this.surveyRepository.create(survey)
 
-    for (const q of questions) {
-      const question = Question.create({
-        questionTitle: q.questionTitle,
-        questionNum: q.questionNum,
-        surveyId: survey.id,
-        accountId: accountIdUnique,
-      })
-
-      await this.questionRepository.create(question)
-
-      for (const opt of q.options) {
-        const option = OptionAnswer.create({
-          optionTitle: opt.optionTitle,
-          optionNum: opt.optionNum,
-          questionId: question.id,
-          accountId: accountIdUnique,
-        })
-
-        await this.optionAnswerRepository.create(option)
-      }
-    }
-
-    return right({ survey })
+    return right({
+      survey,
+    })
   }
 }
